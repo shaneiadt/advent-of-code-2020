e9d828d35483d4435a392d4544b6e5916ba38352
Shane O'Moore <shaneiadt@gmail.com>
2020-12-01 13:24:58 +0000

    day 1 - part 1 success!

--- day-1/index.ts
+++ day-1/index.ts
@@ -1,3 +1,15 @@
 import inputs from './input';
 
-console.log(inputs);
+export const findSum = (sumToFind: number) => (numbers: number[]): [number, number] | null => {
+    for (let x = 0; x <= numbers.length; x++) {
+        for (let y = 0; y <= numbers.length; y++) {
+            if (numbers[x] + numbers[y] === sumToFind) return [numbers[x], numbers[y]];
+        }
+    }
+
+    return null;
+}
+
+const numbers = findSum(2020)(inputs);
+
+console.log({ numbers, multiplied: numbers[0] * numbers[1] });
