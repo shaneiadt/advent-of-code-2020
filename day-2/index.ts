0133accdfd3168d342ff22d28c00987e8bc000e2
Shane O'Moore <shaneiadt@gmail.com>
2020-12-02 15:01:37 +0000

    day 2 - part 2 success!

--- day-2/index.ts
+++ day-2/index.ts
@@ -2,23 +2,38 @@ import inputs from './input';
 
 type Password = string[];
 
-export function getValidPasswords(pwds: Password[]): string[] {
-    const validPwds: string[] = [];
+interface Entry {
+    char: string;
+    pwd: string,
+    min: number;
+    max: number;
+}
+
+const firstPolicyValidator = ({ char, pwd, min, max }: Entry): Boolean => {
+    const pat = new RegExp(`${char}`, 'g');
+    const digits = pwd.match(pat);
+
+    if (digits?.length >= min && digits?.length <= max) return true;
+}
+
+const secondPolicyValidator = ({ char, pwd, min, max }: Entry): Boolean => pwd[min - 1] === char && pwd[max - 1] !== char || pwd[min - 1] !== char && pwd[max - 1] === char && true;
+
+export function getValidPasswords(pwds: Password[], isValid: (entry: Entry) => Boolean): Password {
+    const validPwds: Password = [];
 
     for (const entry of pwds) {
         const [policy, pwd] = entry;
         const [minOccurances, maxOccurances] = policy.match(/\d+/g);
         const char = policy.match(/[a-z]/g)[0];
 
-        const pat = new RegExp(`${char}`, 'g');
-        const digits = pwd.match(pat);
-
-        if (digits?.length >= parseInt(minOccurances) && digits?.length <= parseInt(maxOccurances)) validPwds.push(pwd);
+        if (isValid({ char, pwd, min: parseInt(minOccurances), max: parseInt(maxOccurances) })) validPwds.push(pwd);
     }
 
     return validPwds;
 }
 
-const passwords = getValidPasswords(inputs);
+const partOne = getValidPasswords(inputs, firstPolicyValidator);
+const partTwo = getValidPasswords(inputs, secondPolicyValidator);
 
-console.log(`No. of valid passwords: ${passwords.length}`);
+console.log(`[Part 1] No. of valid passwords: ${partOne.length}`);
+console.log(`[Part 2] No. of valid passwords: ${partTwo.length}`);
