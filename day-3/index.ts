2ce1745aba16cdfb07c4c591ef742b24b0d04c26
Shane O'Moore <shaneiadt@gmail.com>
2020-12-03 13:50:53 +0000

    day 2 - part 1 success!

--- day-3/index.ts
+++ day-3/index.ts
@@ -1,3 +1,22 @@
-// import inputs from './input';
+import { getInputs } from './input';
 
-console.log('DAY 3');
+async function process() {
+    const input = await getInputs();
+    const locations: string[] = [];
+    let pos = 1;
+
+    for (let i = 1; i < input.length; i++) {
+        const row = input[i];
+
+        if (pos + 3 > 31) {
+            pos = (pos + 3) - 31;
+        } else {
+            pos += 3;
+        }
+
+        locations.push(row[pos - 1]);
+    }
+    console.log(`[Part 1] No. of trees encountered: ${locations.filter(loc => loc === '#').length}`);
+}
+
+process();
--- day-3/input.ts
+++ day-3/input.ts
@@ -1,1 +1,22 @@
-export default [];
+import * as fs from 'fs';
+import * as readline from 'readline';
+
+export function getInputs(stream: string = './input.txt'): Promise<string[][]> {
+    const input: string[][] = [];
+
+    return new Promise((resolve, reject) => {
+        try {
+            const file = readline.createInterface({
+                input: fs.createReadStream(stream),
+                output: process.stdout,
+                terminal: false
+            });
+
+            file.on('line', line => input.push(line.split('')));
+            file.on('close', () => resolve(input));
+        } catch (error) {
+            console.error(error);
+            reject(error);
+        }
+    });
+}
