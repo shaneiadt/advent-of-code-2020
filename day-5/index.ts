94fdab425e7c1c5e4a08a962634fbe29bf22fd1f
Shane O'Moore <shaneiadt@gmail.com>
2020-12-05 14:25:43 +0000

    day 5 - part 2 success!

--- day-5/index.ts
+++ day-5/index.ts
@@ -6,25 +6,22 @@ interface BoardingPass {
     sid: number;
 }
 
-const reducer = (upper: string, lower: string) => (tuple: [number, number], letter: string): [number, number] => {
+const reducer = (upper: string) => (tuple: [number, number], letter: string): [number, number] => {
     const midpoint = (tuple[0] + tuple[1]) / 2;
 
-    if (letter === upper) return [Math.ceil(midpoint), tuple[1]];
-
-    return tuple = [tuple[0], Math.floor(midpoint)];
+    return letter === upper ? [Math.ceil(midpoint), tuple[1]] : [tuple[0], Math.floor(midpoint)];
 };
 
 function getBoardingPass(partition: string): BoardingPass {
-    let rowPartition = partition.substring(0, 7);
-    let seatPartition = partition.substring(7);
-
-    const row = rowPartition.split('').reduce(reducer('B', 'F'), [0, 127])[0];
-    const column = seatPartition.split('').reduce(reducer('R', 'L'), [0, 7])[0];
+    const row = partition.substring(0, 7).split('').reduce(reducer('B'), [0, 127])[0];
+    const column = partition.substring(7).split('').reduce(reducer('R'), [0, 7])[0];
     const sid = (row * 8) + column;
 
     return { row, column, sid };
 }
 
-const output = input.map(getBoardingPass);
+const boardingPasses = input.map(getBoardingPass);
+const sid = boardingPasses.map(({ sid }) => sid).sort().filter((sid, i, arr) => sid + 2 === arr[i + 1])[0] + 1;
 
-console.log('[Part 1] What is the highest seat ID on a boarding pass?', output.reduce((highest, { sid }) => sid > highest ? sid : highest, 0));
+console.log('[Part 1] What is the highest seat ID on a boarding pass?', boardingPasses.reduce((highest, { sid }) => sid > highest ? sid : highest, 0));
+console.log('[Part 2] What is the ID of your seat?', sid);
