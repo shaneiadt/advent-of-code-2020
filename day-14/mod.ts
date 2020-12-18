a2e10e2f12aeab95a63f449713ec65d9442ddc6b
Shane O'Moore <shaneiadt@gmail.com>
2020-12-18 21:06:34 +0000

    day 14 - part 1 success!

--- day-14/mod.ts
+++ day-14/mod.ts
@@ -1,11 +1,17 @@
 import { data, sample } from "./data.ts";
-import { getInputs, getMask, toBinary } from "./utils.ts";
+import {
+  applyBitMask,
+  getInputs,
+  getMask,
+  getMemoryAddress,
+  toBinary,
+} from "./utils.ts";
 import { Input, Program } from "./interfaces.ts";
 
 let programs: Program[] = [];
 let pid: number;
 
-sample.split("\n").forEach((line, index) => {
+data.split("\n").forEach((line, index) => {
   if (line.includes("mask")) {
     pid = index;
 
@@ -22,15 +28,31 @@ sample.split("\n").forEach((line, index) => {
 
 programs = programs.sort((a, b) => a.id - b.id);
 
+let memory: { [key: string]: number } = {};
+
 function run(program: Program) {
-  const mask = getMask(program.mask);
   const inputs: Input[] = getInputs(program.input);
 
-  console.log(mask, inputs);
+  for (const input of inputs) {
+    const address = getMemoryAddress(input.address);
+    const value = toBinary(input.value);
+    const mask = getMask(program.mask);
+    const result = applyBitMask(mask, value);
+
+    memory = {
+      ...memory,
+      [address]: parseInt(result, 2),
+    };
+  }
 }
 
 for (const program of programs) {
   run(program);
 }
 
-// console.log(programs);
+const sum = Object.values(memory).reduce((total, val) => total + val);
+
+console.log(
+  "[Part 1] What is the sum of all values left in memory after it completes?",
+  sum,
+);
--- day-14/utils.ts
+++ day-14/utils.ts
@@ -1,26 +1,22 @@
-export const toBinary = (integer: number, padding: number): string =>
+import { Input } from "./interfaces.ts";
+
+export const toBinary = (integer: number, padding: number = 36): string =>
   integer.toString(2).padStart(padding, "0");
 
 export const getMask = (mask: string): string => mask.replace("mask = ", "");
 
-interface IInput {
-  address: string;
-  value: number;
-}
-
 export const getInputs = (
   array: string[],
-): IInput[] => {
+): Input[] => {
   const copy = [...array];
-  const result: IInput[] = [];
+  const result: Input[] = [];
 
   if (copy) {
     for (let i = 0; i < copy.length; i++) {
-      const index = Number(i);
       const val = copy[i] || "";
       const split = val.split(" = ");
 
-      const item: IInput = {
+      const item: Input = {
         address: split[0],
         value: Number(split[1]),
       };
@@ -30,4 +26,29 @@ export const getInputs = (
   }
 
   return result;
+};
+
+export const getMemoryAddress = (str: string): string => {
+  const regExp = str.match(/\d+/g);
+  const result = regExp ? regExp[0] : "";
+  
+  return result;
+};
+
+export const applyBitMask = (mask: string, value: string): string => {
+  let result = value.split("");
+
+  for (let i = 0; i < mask.length; i++) {
+    if (mask[i] !== "X") {
+      const digit = Number(mask[i]);
+
+      if (digit === 1) {
+        result[i] = "1";
+      } else if (digit === 0) {
+        result[i] = "0";
+      }
+    }
+  }
+
+  return result.join("");
 };
