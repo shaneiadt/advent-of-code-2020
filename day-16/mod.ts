8ebc3032f2947ff7501e881f75c25d20317cf29b
Shane O'Moore <shaneiadt@gmail.com>
2020-12-22 11:32:27 +0000

    add todo

--- day-16/mod.ts
+++ day-16/mod.ts
@@ -15,6 +15,10 @@ const nearbyTicketsObj = getNearbyTickets(dataNearbyTickets);
 
 const invalidTickets = validateTicketsByFields(nearbyTicketsObj, rangeFields);
 
+console.log(invalidTickets);
+
+// TODO: Filter out all invalid tickets
+
 console.log(
   "[Part 1] Consider the validity of the nearby tickets you scanned. What is your ticket scanning error rate?",
   invalidTickets.reduce((total, cur) => total + cur, 0),
